32476d2dd3d35c7bdf1eaac995f0dc31f60aaf10
Bruce Wayne <miao17game@qq.com>
2017-12-30 14:15:45 +0800

    update projects

---
+++ src/@angular/core/index.ts
@@ -0,0 +1,4 @@
+import * as angular from "angular";
+
+export const CoreModule = angular.module("angular-x-v1", []);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const webpack = require("webpack");
 
 module.exports = {
-
+    entry: {
+        index: "./src/index.ts"
+    }
 }
