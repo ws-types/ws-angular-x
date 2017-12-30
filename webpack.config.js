932b801d6ee209844f53249bf7fb7612ac1cd0df
Bruce Wayne <miao17game@qq.com>
2017-12-30 14:43:28 +0800

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,67 @@
 const path = require('path');
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
+        vendor: ["angular", "bootstrap", "jquery"],
         index: "./src/index.ts"
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+        publicPath: "/assets/",
+        // library: "ws-angular-x",
+        // libraryTarget: "umd",
+        pathinfo: true,
+        chunkFilename: "[name].chunk.js",
+        sourceMapFilename: "[file].map",
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: "./index.html"
+        })
+    ],
+    resolve: {
+        modules: [
+            "node_modules",
+            path.resolve(__dirname, "src")
+        ],
+        extensions: [".js", ".json", ".ts"],
+        alias: {
+            "@angular": path.resolve(__dirname, "src/@angular"),
+            "@src": path.resolve(__dirname, "src/web-test")
+        },
+    },
+    devtool: "inline-source-map",
+    performance: {},
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, "src/web-test")
+                ],
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
     }
 }
